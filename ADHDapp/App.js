58abd0b9443403270bce809b969d26aec9215866
Anastasia Khizgiyaev <nastiakh28@gmail.com>
2023-05-23 01:48:30 +0300

    update the navigation of settings in the App

--- ADHDapp/App.js
+++ ADHDapp/App.js
@@ -1,3 +1,123 @@
+// import { useState, useEffect, useRef } from "react";
+// import { Text, View, Button, Platform } from "react-native";
+// import * as Device from "expo-device";
+// import * as Notifications from "expo-notifications";
+
+// Notifications.setNotificationHandler({
+//   handleNotification: async () => ({
+//     shouldShowAlert: true,
+//     shouldPlaySound: false,
+//     shouldSetBadge: false,
+//   }),
+// });
+
+// // Can use this function below OR use Expo's Push Notification Tool from: https://expo.dev/notifications
+// async function sendPushNotification(expoPushToken) {
+//   const message = {
+//     to: expoPushToken,
+//     sound: "default",
+//     title: "Original Title",
+//     body: "And here is the body!",
+//     data: { someData: "goes here" },
+//   };
+
+//   await fetch("https://exp.host/--/api/v2/push/send", {
+//     method: "POST",
+//     headers: {
+//       Accept: "application/json",
+//       "Accept-encoding": "gzip, deflate",
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(message),
+//   });
+// }
+
+// async function registerForPushNotificationsAsync() {
+//   let token;
+//   if (Device.isDevice) {
+//     const { status: existingStatus } =
+//       await Notifications.getPermissionsAsync();
+//     let finalStatus = existingStatus;
+//     if (existingStatus !== "granted") {
+//       const { status } = await Notifications.requestPermissionsAsync();
+//       finalStatus = status;
+//     }
+//     if (finalStatus !== "granted") {
+//       alert("Failed to get push token for push notification!");
+//       return;
+//     }
+//     token = (await Notifications.getExpoPushTokenAsync()).data;
+//     console.log(token);
+//   } else {
+//     alert("Must use physical device for Push Notifications");
+//   }
+
+//   if (Platform.OS === "android") {
+//     Notifications.setNotificationChannelAsync("default", {
+//       name: "default",
+//       importance: Notifications.AndroidImportance.MAX,
+//       vibrationPattern: [0, 250, 250, 250],
+//       lightColor: "#FF231F7C",
+//     });
+//   }
+
+//   return token;
+// }
+
+// export default function App() {
+//   const [expoPushToken, setExpoPushToken] = useState("");
+//   const [notification, setNotification] = useState(false);
+//   const notificationListener = useRef();
+//   const responseListener = useRef();
+
+//   useEffect(() => {
+//     registerForPushNotificationsAsync().then((token) =>
+//       setExpoPushToken(token)
+//     );
+
+//     notificationListener.current =
+//       Notifications.addNotificationReceivedListener((notification) => {
+//         setNotification(notification);
+//       });
+
+//     responseListener.current =
+//       Notifications.addNotificationResponseReceivedListener((response) => {
+//         console.log(response);
+//       });
+
+//     return () => {
+//       Notifications.removeNotificationSubscription(
+//         notificationListener.current
+//       );
+//       Notifications.removeNotificationSubscription(responseListener.current);
+//     };
+//   }, []);
+
+//   return (
+//     <View
+//       style={{ flex: 1, alignItems: "center", justifyContent: "space-around" }}
+//     >
+//       <Text>Your expo push token: {expoPushToken}</Text>
+//       <View style={{ alignItems: "center", justifyContent: "center" }}>
+//         <Text>
+//           Title: {notification && notification.request.content.title}{" "}
+//         </Text>
+//         <Text>Body: {notification && notification.request.content.body}</Text>
+//         <Text>
+//           Data:{" "}
+//           {notification && JSON.stringify(notification.request.content.data)}
+//         </Text>
+//       </View>
+//       <Button
+//         title="Press to Send Notification"
+//         onPress={async () => {
+//           await sendPushNotification(expoPushToken);
+//         }}
+//       />
+//     </View>
+//   );
+// }
+
 // import * as React from "react";
 // import { NavigationContainer } from "@react-navigation/native";
 // import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -24,18 +144,26 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
 import Settings from "./components/settings";
 import HomePage from "./components/home";
 import Score from "./components/score";
 import Login from "./components/login";
-import PoppingNumbers from "./components/popping_numbers";
+import Appearance from "./screens/appearance";
+import TextSize from "./screens/font_size";
+import Contact from "./screens/contact";
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    Cocogoose: require("./assets/fonts/Cocogoose.ttf"),
+    Cocogoose_light: require("./assets/fonts/Cocogoose_light.ttf"),
+  });
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen
           name="Login"
           component={Login}
@@ -57,9 +185,19 @@ const App = () => {
           options={{ title: "Score" }}
         />
         <Stack.Screen
-          name="PoppingNumbers"
-          component={PoppingNumbers}
-          options={{ title: "Popping" }}
+          name="Contact"
+          component={Contact}
+          options={{ title: "contact" }}
+        />
+        <Stack.Screen
+          name="Appearance"
+          component={Appearance}
+          options={{ title: "appearance" }}
+        />
+        <Stack.Screen
+          name="Text Size"
+          component={TextSize}
+          options={{ title: "Text Size" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
